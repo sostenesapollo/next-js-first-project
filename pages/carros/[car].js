18e9a553552cb18117666a25e50c0176fd55933b
sostenesapollo <sostenesapollo25@gmail.com>
2020-11-18 14:42:56 -0300

    Seo

---
+++ pages/carros/[car].js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+import { NextSeo } from 'next-seo';
+
+export default function Cars({car}) {
+    var router = useRouter()    
+    return (
+        <>
+            <NextSeo
+                title={"Car: "+router.query.car}
+                description="Then with a short description here."
+            />
+            Carro nome:   {router.query.car}
+
+            <p>
+                <Link href="/">Home</Link>
+            </p>
+        </>
+    )
+}
